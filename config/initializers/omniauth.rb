eaaab86c923c66702cad67568d94e2cebd613e45
Thomas Ricard <thomas.ricard@gmail.com>
2014-03-17 23:32:42 -0700

    Seems like github won't let me access private organization repos unless I set the oauth scope to repos instead of repos:status

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, '2af9f9b4dfa6762ba0a4', '426a71e9f4e7037c6cdb5d79360e8fc281460b63', scope: "user:email,repo:status,read:org"
+  provider :github, '2af9f9b4dfa6762ba0a4', '426a71e9f4e7037c6cdb5d79360e8fc281460b63', scope: "user:email,repo,read:org"
 end
