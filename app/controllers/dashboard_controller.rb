0a1442d6d64612359ee01fafde5d40117cd20830
Thomas Ricard <thomas.ricard@gmail.com>
2014-03-17 23:31:05 -0700

    automatically choose for now the org/repo of the authenticated user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,18 @@
 class DashboardController < ApplicationController
 	def show
 		if(@current_user.organization_name.nil?)
-			@organizations = @github.orgs.all
+			organizations = @github.orgs.all
+			@orgs = {}
+
+			organizations.each  do |org|
+				@orgs[org.login] = @github.repos(org: org.login).list(type: 'private').map { |e|  e.name}
+			end
+
+			# for now, I just put the first repo of the first organization as the one used
+			@current_user.organization_name = @orgs.keys.first
+			@current_user.organization_id = @orgs.keys.first
+			@current_user.repo= @orgs[@orgs.keys.first][0]
+			@current_user.save!
 		end
-
-
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315062809) do
+ActiveRecord::Schema.define(version: 20140318061514) do
 
   create_table "commits", force: true do |t|
     t.string   "hash"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140315062809) do
     t.string   "organization_id"
     t.string   "organization_name"
     t.string   "api_token"
+    t.string   "repo"
   end
 
 end
